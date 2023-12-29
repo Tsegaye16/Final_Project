064906cff11876260e3f7dd50dc6f8f56ba4e627
Tsegaye16 <abewatsegaye16@gmail.com>
2023-12-30 00:58:54 +0300

    add style

--- client/src/components/Student/student_board.js
+++ client/src/components/Student/student_board.js
@@ -1,27 +1,27 @@
-// dash_board.js
-import React,{useState} from 'react';
-import './student_board.css'; // Make sure to import the CSS file
-import Side_bar from '../sidebar/side_bar';
+// // dash_board.js
+// import React,{useState} from 'react';
+// import './student_board.css'; // Make sure to import the CSS file
+// import Side_bar from '../sidebar/side_bar';
 
 
 
-function StudentBoard() {
+// function StudentBoard() {
     
     
 
   
   
-  return (
-    <div className="dashboard-container">
-      <Side_bar toggleSidebar={toggleSidebar} sidebarWidth={sidebarWidth} setSelectedItem={setSelectedItem} />
-      <div className="main-content" >     
+//   return (
+//     <div className="dashboard-container">
+//       <Side_bar toggleSidebar={toggleSidebar} sidebarWidth={sidebarWidth} setSelectedItem={setSelectedItem} />
+//       <div className="main-content" >     
      
-       {renderSelectedComponent() }
+//        {renderSelectedComponent() }
 
        
-      </div>
-    </div>
-  );
-}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default StudentBoard;
+// export default StudentBoard;
