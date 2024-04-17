ebdbbf9f72877ed0a7f28e9b3341f37dd24bd6ba
Tsegaye16 <abewatsegaye16@gmail.com>
2024-04-17 08:50:47 +0300

    Isolate tools

--- server/admin/viewInstructor.js
+++
@@ -1,9 +0,0 @@
-const viewInstructor = (db, req, res) => {
-  const instructorData =
-    "SELECT instructor_id, users.* FROM instructors JOIN users ON instructors.user_id = users.user_id;";
-  db.query(instructorData, (err, result) => {
-    //console.log(result)
-    res.send(result);
-  });
-};
-export default viewInstructor;
